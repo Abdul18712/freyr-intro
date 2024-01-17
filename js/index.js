fb7bee93504024edeb7fce5af6080ca5e4b1b1a1
Abdul18712 <lanval.danladi@gmail.com>
2024-01-16 17:35:46 -0800

    heyperlink texts

--- js/index.js
+++ js/index.js
@@ -45,25 +45,23 @@ messageForm.addEventListener('submit', function(event) {
     messageForm.reset();
 });
 
-var githubRequest = new XMLHttpRequest ();
+var githubRequest = new XMLHttpRequest();
 githubRequest.open('GET', "https://api.github.com/users/Abdul18712/repos");
 githubRequest.send();
 githubRequest.addEventListener('load', function () {
-    var repositories = JSON.parse(this.response)
-    // console.log(repositories);
-    var projectSection = document.querySelector("#projects");
-    var projectList = projectSection.querySelector("ul");
-    for (var i = 0; i < repositories.length; i++) {
-        var project = document.createElement("li");
-        var link = document.createElement("a");
-        project.innerText = repositories[i].name;
-        link.href = repositories[i].html_url;
-        link.target = "_blank";
-        projectList.appendChild(project);
-        project.appendChild(link);
-        var description = document.createElement("p");
-        description.innerText = repositories[i].description;
-        project.appendChild(description);
-        
-    }
+  var repositories = JSON.parse(this.response);
+  var projectSection = document.querySelector("#projects");
+  var projectList = projectSection.querySelector("ul");
+  for (var i = 0; i < repositories.length; i++) {
+    var project = document.createElement("li");
+    var link = document.createElement("a");
+    link.innerText = repositories[i].name; 
+    link.href = repositories[i].html_url;
+    link.target = "_blank";
+    project.appendChild(link);
+    projectList.appendChild(project);
+    var description = document.createElement("p");
+    description.innerText = repositories[i].description;
+    project.appendChild(description);
+  }
 });
